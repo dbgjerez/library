8e9a9918f774f7de676a94a3271848ec35c31703
David Borrego Gutiérrez <dborrego@atsistemas.com>
2016-08-25 15:13:48 +0200

    clase para el usuario

---
+++ library/src/main/java/com/at/library/model/User.java
@@ -0,0 +1,46 @@
+package com.at.library.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class User implements Serializable {
+
+	private static final long serialVersionUID = 4877910540692673267L;
+
+	@Id
+	@GeneratedValue
+	private Integer id;
+
+	private String name;
+
+	private String dni;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+}
