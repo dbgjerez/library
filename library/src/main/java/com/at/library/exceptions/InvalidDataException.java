58e820fb41638a211a9e6204ed3c9c2916427c71
David Borrego Gutiérrez <dborrego@atsistemas.com>
2016-08-28 11:14:21 +0200

    excepción para cuando los datos para crear no sean correctos

---
+++ library/src/main/java/com/at/library/exceptions/InvalidDataException.java
@@ -0,0 +1,11 @@
+package com.at.library.exceptions;
+
+public class InvalidDataException extends Exception {
+
+	private static final long serialVersionUID = -4039022429943225379L;
+
+	public InvalidDataException(String message) {
+		super(message);
+	}
+
+}
