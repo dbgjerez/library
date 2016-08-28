3e88c4fb8405db0e3a83eb823680181ca369c1de
David Borrego Gutiérrez <dborrego@atsistemas.com>
2016-08-28 11:14:54 +0200

    dto para la creació  de usuarios

---
+++ library/src/main/java/com/at/library/dto/user/UserPostDTO.java
@@ -0,0 +1,34 @@
+package com.at.library.dto.user;
+
+import com.at.library.dto.DTO;
+
+public class UserPostDTO extends DTO {
+
+	private static final long serialVersionUID = -3766236403591774937L;
+
+	protected String dni;
+
+	protected String name;
+
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "UserPostDTO [dni=" + dni + ", name=" + name + "]";
+	}
+
+}
