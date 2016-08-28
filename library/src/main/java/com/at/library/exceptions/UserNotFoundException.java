96ebf286a68556dbb695ce9bccc58227b241ef25
David Borrego Gutiérrez <dborrego@atsistemas.com>
2016-08-28 11:18:41 +0200

    excepción para cuando no se encuentra un usuario

--- library/src/main/java/com/at/library/exceptions/UserNotFoundException.java
+++ library/src/main/java/com/at/library/exceptions/UserNotFoundException.java
@@ -4,8 +4,10 @@ public class UserNotFoundException extends NotFoundException {
 
 	private static final long serialVersionUID = -3777473523291657124L;
 
-	public UserNotFoundException(String message) {
-		super(message);
+	private static final String MSG = "El usuario no existe";
+
+	public UserNotFoundException() {
+		super(MSG);
 	}
 
 }
