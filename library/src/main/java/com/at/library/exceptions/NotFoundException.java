faad486fca13f895c621deaa7f4ce1a75ce73636
David Borrego Gutiérrez <dborrego@atsistemas.com>
2016-08-28 11:14:34 +0200

    excepción para cuando no se encuentre algo en la aplicación

---
+++ library/src/main/java/com/at/library/exceptions/NotFoundException.java
@@ -0,0 +1,11 @@
+package com.at.library.exceptions;
+
+public class NotFoundException extends Exception {
+
+	private static final long serialVersionUID = -7406696324422023919L;
+
+	public NotFoundException(String message) {
+		super(message);
+	}
+
+}
