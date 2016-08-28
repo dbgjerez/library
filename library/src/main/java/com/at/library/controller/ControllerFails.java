f6bc8cf631ce2bb71311e6704ba27265e8eabc8b
David Borrego Gutiérrez <dborrego@atsistemas.com>
2016-08-28 11:55:56 +0200

    controller advice para los errores genéricos de la aplicación

---
+++ library/src/main/java/com/at/library/controller/ControllerFails.java
@@ -0,0 +1,22 @@
+package com.at.library.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import com.at.library.dto.ApiErrorDTO;
+import com.at.library.exceptions.UserNotFoundException;
+
+@ControllerAdvice(basePackages = { "com.at.library.controller" })
+public class ControllerFails {
+
+	@ResponseBody
+	@ExceptionHandler(UserNotFoundException.class)
+	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+	public ApiErrorDTO error(Exception e) {
+		return new ApiErrorDTO(404, e.getMessage());
+	}
+
+}
