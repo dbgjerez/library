8deb4f4892b0ba60b371f9eb142dac0baeca1d2d
David Borrego Gutiérrez <dborrego@atsistemas.com>
2016-08-28 12:16:17 +0200

    añado excepción para bad request

--- library/src/main/java/com/at/library/controller/ControllerFails.java
+++ library/src/main/java/com/at/library/controller/ControllerFails.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 import com.at.library.dto.ApiErrorDTO;
+import com.at.library.exceptions.InvalidDataException;
 import com.at.library.exceptions.UserNotFoundException;
 
 @ControllerAdvice(basePackages = { "com.at.library.controller" })
@@ -17,6 +18,13 @@ public class ControllerFails {
 	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
 	public ApiErrorDTO error(Exception e) {
 		return new ApiErrorDTO(404, e.getMessage());
+	}
+
+	@ResponseBody
+	@ExceptionHandler(InvalidDataException.class)
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	public ApiErrorDTO error(InvalidDataException e) {
+		return new ApiErrorDTO(400, e.getMessage());
 	}
 
 }
